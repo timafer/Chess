778a054c34bb18e52db6a952430989daa1fab80f
timafer <boomer3000@comcast.net>
2016-07-26 19:46:23 -0600

    Basic Board

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace Chess
     /// </summary>
     public partial class MainWindow : Window
     {
+        Label[,] squares= new Label[8, 8];
+        const int boardsize= 8;
+        int coloralt = 0;
         public MainWindow()
         {
             InitializeComponent();
+            for (int i=0;i<boardsize;i++)
+            {
+                for (int o = 0; o < boardsize; o++)
+                {
+                    squares[o, i] = new Label();
+                    board.Children.Add(squares[o, i]);
+                    squares[o, i].SetValue(Grid.RowProperty, o);
+                    squares[o, i].SetValue(Grid.ColumnProperty, i);
+                    if (coloralt==0)
+                    {
+                        squares[o, i].Background = Brushes.Black;
+                        coloralt++;
+                    }
+                    else
+                    {
+                        squares[o, i].Background = Brushes.White;
+                        coloralt = 0;
+                    }
+                }
+                //makes it so that the columns and rows dant all match
+                if (coloralt == 0)
+                {
+                    coloralt++;
+                }
+                else
+                {
+                    coloralt = 0;
+                }
+            }
         }
     }
 }
